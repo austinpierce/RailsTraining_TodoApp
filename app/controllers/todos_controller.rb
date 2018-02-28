0f2d50fdb885a1813d893016fd8d370fbe7025b4
austinpierce <austinpierce@gmail.com>
2018-02-27 22:37:09 -0500

    Add if / redirect, error handeling, validation

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -7,8 +7,16 @@ class TodosController < ApplicationController
   
   def create
     @todo = Todo.new(todo_params) # instance variable
+    if @todo.save # save to DB
+    redirect_to todo_path(@todo)   
+    else
+      render 'new'
+    end
   end
   
   private # private means only available to this controller
+    def todo_params
+      params.require(:todo).permit(:name, :description) #white listing
+    end
   
 end
